0df621cccc3e3e784c9db0754e06da66f7e78da0
Andrew <arhorn@smcm.edu>
2019-04-01 17:21:48 -0500

    function now takes a second of the hour

--- getLatLongAroundHouston.js
+++ getLatLongAroundHouston.js
@@ -1,18 +1,19 @@
-const getLatLongAroundHouston = (radius, timeUnit) => {
-  // angle in radians
-  const theta = (-timeUnit / 450) * Math.PI;
+const getLatLongAroundHouston = secondOfTheHour => {
+  const theta = (-secondOfTheHour / 450) * Math.PI;
+  const radius = 5; // about 500km
   const x = radius * Math.cos(theta);
   const y = radius * Math.sin(theta);
-  const lat = x - 95.3698;
-  const long = y + 29.7604;
+  const lat = y + 29.761993;
+  const long = x - 95.366302;
   return { lat, long };
 };
 
-const assert = require("assert");
+const assert = require('assert');
 
-const radius = 10;
-const timeUnit = 0;
+const secondOfTheHour = 0;
 
-console.log(getLatLongAroundHouston(radius, timeUnit));
+const result = getLatLongAroundHouston(radius, secondOfTheHour);
+
+console.log(`${result.lat},${result.long}`);
 
 // assert.equal(getLatLongAroundHouston(radius, angle));
